b7c3ede82e97e7b0720d522db60b461b5abd3c53
PierreGoude <linoge80@live.se>
2016-09-22 23:18:28 +0200

    Update Deployserver.sh

--- Deployserver.sh
+++ Deployserver.sh
@@ -11,16 +11,37 @@
 #This skript is not finished!
 
 
-############## deploy server installation##################
+##############3 deploy server installation##################
 cd /etc/apache2/mods-enabled
 $ sudo ln -s ../mods-available/cgi.load
 
- sudo apt-get install curl -y
- sudo apt-get install apache2 -y
- sudo a2enmod cgi -y
+ sudo apt-get install curl
+ 
+ sudo apt-get install apache2
+ sudo a2enmod cgi
  sudo service apache2 reload
-sudo echo '
+ 
+ 
+ sudo echo '#Defaults for tftpd-hpa
 RUN_DAEMON="yes"
 OPTIONS="-l -s /var/lib/tftpboot"' >> /etc/default/tftpd-hpa
 /etc/init.d/tftpd-hpa restart
+sudo mkdir /var/www/html/ubuntu
+sudo rm -rf /var/www/html/index.html
+sudo mkdir /var/lib/tftpboot/ubuntu
+
+sudo echo '
+allow booting;
+allow bootp;
+
+subnet 192.168.2.0 netmask 255.255.255.0 {
+  range 192.168.2.xxx 192.168.2.xxx;
+  option broadcast-address 192.168.2.255;
+  option routers 192.168.2.xxx;
+  option domain-name-servers 192.168.2.xxx;
+
+  filename "pxelinux.0";
+}' >> /etc/dhcp/dhcpd.conf
+sudo cp /usr/lib/syslinux/pxelinux.0 /var/lib/tftpboot/
+sudo echo 'tftp dgram udp wait root /usr/sbin/in.tftpd /usr/sbin/in.tftpd -s /var/lib/tftpboot' >> /etc/inetd.conf
 
