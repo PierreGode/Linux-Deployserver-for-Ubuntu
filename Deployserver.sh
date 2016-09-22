6b762a08d12f5425c34f34a8112ca6c3cfd75259
PierreGoude <linoge80@live.se>
2016-09-22 23:08:37 +0200

    Update Deployserver.sh

--- Deployserver.sh
+++ Deployserver.sh
@@ -15,14 +15,11 @@
 cd /etc/apache2/mods-enabled
 $ sudo ln -s ../mods-available/cgi.load
 
- sudo apt-get install curl
- 
- sudo apt-get install apache2
- sudo a2enmod cgi
+ sudo apt-get install curl -y
+ sudo apt-get install apache2 -y
+ sudo a2enmod cgi -y
  sudo service apache2 reload
- 
- 
- sudo echo '#Defaults for tftpd-hpa
+sudo echo '
 RUN_DAEMON="yes"
 OPTIONS="-l -s /var/lib/tftpboot"' >> /etc/default/tftpd-hpa
 /etc/init.d/tftpd-hpa restart
