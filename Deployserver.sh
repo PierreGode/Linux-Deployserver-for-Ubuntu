7457a177dc5f0e4bc71758a671466e53a025a717
PierreGoude <linoge80@live.se>
2016-09-23 13:12:06 +0200

    Update Deployserver.sh

--- Deployserver.sh
+++ Deployserver.sh
@@ -11,12 +11,18 @@
 #This skript is not finished!
 
 ############## deploy server installation##################
+clear
+echo "Making neccesery installs"
+sleep 2
 sudo apt-get update
 sudo apt-get install curl -y
 sudo apt-get install apache2 -y
 sudo a2enmod cgi -y
 sudo apt-get install isc-dhcp-server -y
 sudo service apache2 reload
+clear
+echo "Configurating..."
+sleep 1
 sudo echo '
 RUN_DAEMON="yes"
 OPTIONS="-l -s /var/lib/tftpboot"' >> /etc/default/tftpd-hpa
@@ -40,7 +46,8 @@ while true; do
    esac
 done
 else
-echo "Writing to dhcp.conf"
+clear
+echo "Setting up dhcp.conf"
 fi
 echo "Type IP"
 read depip
@@ -71,14 +78,15 @@ sudo update-inetd --enable BOOT
 sudo service tftpd-hpa restart
 sudo cp /usr/lib/syslinux/vesamenu.c32 /var/lib/tftpboot/
 sudo cp /usr/lib/syslinux/pxelinux.0 /var/lib/tftpboot/
+sudo echo "Downloading Ubuntu16 image from archive"
 sudo wget http://archive.ubuntu.com/ubuntu/dists/xenial-updates/main/installer-amd64/current/images/netboot/netboot.tar.gz´
 sudo gunzip *.gz
 sudo tar -xvf *.tar
 clear
 echo "Primary settings has been setup.. please check your dhcp conf for errors"
 echo "Remember to set bootfile name to /var/lib/tftpboot/pxelinux.0 in your dhcp"
-echo "upload kickstart file to /var/www/ubuntu/ and cofigure (example below) /var/lib/tftpboot/ubuntu-installer/amd64/boot-screens/txt.cfg"
+echo "upload kickstart file to /var/www/ubuntu/ and cofigure (example below) in /var/lib/tftpboot/ubuntu-installer/amd64/boot-screens/txt.cfg"
 echo "'label install
-        menu label ^Ubuntu 14 Gen
+        menu label ^Ubuntu 16 Gen
         kernel ubuntu-installer/amd64/linux
-        append ks=http://10.46.21.53/ubuntugeneric.cfg vga=788 initrd=ubuntu-installer/amd64/initrd.gz --- quiet'"
+        append ks=http://192.168.1.5/ubuntugeneric.cfg vga=788 initrd=ubuntu-installer/amd64/initrd.gz --- quiet'"
